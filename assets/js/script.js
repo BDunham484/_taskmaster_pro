85856bdf8b8365479663fc936d943d72ff624343
ThereWolf <bdunham484@gmail.com>
2022-06-02 15:11:27 -0500

    add draggable and sortable

--- assets/js/script.js
+++ assets/js/script.js
@@ -229,10 +229,52 @@ $(".card .list-group").sortable({
     console.log("out", event.target);
   },
   update: function(event) {
-    console.log("update", this);
+    //array to store the task data in
+    var tempArr = [];
+    //loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+      //add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+     //trim down list's ID to match object property
+     var arrName = $(this)
+     .attr("id")
+     .replace("list-", "");
+
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+    console.log(tempArr);
   }
-  
+});
 
 
 
+
+//trash icon can be dropped onto
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("DROP")
+    //remove dragged element from the dom
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
 });
